7269a0d5873065a184e57914297abaa921e5974e
Debora Baddipudi <64851336+baddipudiDebora@users.noreply.github.com>
2025-05-22 15:38:12 -0400

    Update SenarioOneTest.java

--- src/test/java/demo/display/Single_Responsibility_DesignPattern/SenarioOneTest.java
+++ src/test/java/demo/display/Single_Responsibility_DesignPattern/SenarioOneTest.java
@@ -45,9 +45,9 @@ public class SenarioOneTest {
         LandingPage lp = new LandingPage(driver);
 
         lp.loadLandingPage();
-		  Thread.sleep(3000);
+		  Thread.sleep(9000);
         System.out.println("loaded landing page succesfully");
-         //   lp.closePopUp();
+            lp.closePopUp();
 		  lp.getFooterBar().selectdomesticHotels().click();
 
 		  lp.getNavigationBar().selectHotels();
